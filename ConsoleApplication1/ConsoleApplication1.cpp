47c8f306f13fc46a9f145e035b52a52480ecfb5f
Linismile1331 <101728608+Linismile1331@users.noreply.github.com>
2023-12-02 20:02:31 +0200

    third commit

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,22 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello ME!\n";
+int main() {
+    double a, b, c;
+    cout << "¬вед≥ть довжину сторони a: ";
+    cin >> a;
+    cout << "¬вед≥ть довжину сторони b: ";
+    cin >> b;
+    cout << "¬вед≥ть довжину сторони c: ";
+    cin >> c;
+
+    double s = (a + b + c) / 2;
+    double area = sqrt(s * (s - a) * (s - b) * (s - c));
+    double h_c = 2 * area / c;
+    double m_a = sqrt(2 * (b * b + c * c) - a * a) / 2;
+
+    cout << "¬исота h_c: " << h_c << endl;
+    cout << "ћед≥ана m_a: " << m_a << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
